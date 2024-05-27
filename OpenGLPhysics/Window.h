2ac909005e93d9e320aa05e310f82956bfe4d7b3
MikaelMarkussen <244604@stud.inn.no>
2024-05-27 23:04:36 +0200

    First commit

---
+++ OpenGLPhysics/Window.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <GLFWa>
+
+class Window
+{
+public:
+	Window();
+	~Window();
+	void run()
+	{
+		
+	}
+private:
+
+};
+
+Window::Window()
+{
+}
+
+Window::~Window()
+{
+}
---
+++ OpenGLPhysics/main.cpp
@@ -0,0 +1,8 @@
+﻿#include <filesystem>
+
+int main()
+{
+#ifdef PROJECT_DIR
+	std::filesystem::current_path(PROJECT_DIR);
+#endif // PROJECT_DIR
+}
